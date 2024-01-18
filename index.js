4838ab9461798d018f2d03a41268c8eecc51cdb8
francosromano <francosromano.1994@gmail.com>
2024-01-17 23:48:43 -0300

    Tercera preentrega

--- index.js
+++ index.js
@@ -1,153 +1,153 @@
 /*********************                        Alert de ingreso                       ************************ */
 /*                                (Variables + constantes + Do..While + Condicionales)                        */
 
-// const COMISION = 49810;
-// let continuar
+const COMISION = 49810;
+let continuar
 
-// do{
-//     let ingreso = Number(prompt("¡Hola!\ Ingrese número de comisión para ingresar:"));
+do{
+    let ingreso = Number(prompt("¡Hola!\ Ingrese número de comisión para ingresar:"));
 
-//     if(ingreso===COMISION){
-//         alert("Bienvenido Milton Salazar!")
-//         break;
-//     }else{
-//         alert("Número de comisión incorrecto")
-//         continuar = prompt("¿Desea seguir intentando?").toLowerCase()
-//         if (continuar === "no"){
-//             alert("¡En otra ocasión lo intentaremos")
-//     }
-// }
-// } while(continuar==="si");
+    if(ingreso===COMISION){
+        alert("Bienvenido Milton Salazar!")
+        break;
+    }else{
+        alert("Número de comisión incorrecto")
+        continuar = prompt("¿Desea seguir intentando?").toLowerCase()
+        if (continuar === "no"){
+            alert("¡En otra ocasión lo intentaremos")
+    }
+}
+} while(continuar==="si");
 
 // /*********************************             Calculadora                   ************************************/
 // /*                                         (Funciones + Switch)                                                 */
 
 
-// function sumar() {
-//     const NUMERO_A = Number(prompt("Ingrese el primer número para sumar"));
-//     const NUMERO_B = Number(prompt("Ingrese el segundo número para sumar"));
-//     const RESULTADO = NUMERO_A + NUMERO_B
-//     alert(NUMERO_A + " + " + NUMERO_B + " = " + RESULTADO)
-//     console.log("El resultado de " + NUMERO_A + " + " + NUMERO_B + " es " + RESULTADO)
-// }
+function sumar() {
+    const NUMERO_A = Number(prompt("Ingrese el primer número para sumar"));
+    const NUMERO_B = Number(prompt("Ingrese el segundo número para sumar"));
+    const RESULTADO = NUMERO_A + NUMERO_B
+    alert(NUMERO_A + " + " + NUMERO_B + " = " + RESULTADO)
+    console.log("El resultado de " + NUMERO_A + " + " + NUMERO_B + " es " + RESULTADO)
+}
 
-// function restar() {
-//     const NUMERO_A = Number(prompt("Ingrese el primer número para restar"));
-//     const NUMERO_B = Number(prompt("Ingrese el segundo número para restar"));
-//     const RESULTADO = NUMERO_A - NUMERO_B
-//     alert(NUMERO_A + " - " + NUMERO_B + " = " + RESULTADO)
-//     console.log("El resultado de " + NUMERO_A + " - " + NUMERO_B + " es " + RESULTADO)
-// }
+function restar() {
+    const NUMERO_A = Number(prompt("Ingrese el primer número para restar"));
+    const NUMERO_B = Number(prompt("Ingrese el segundo número para restar"));
+    const RESULTADO = NUMERO_A - NUMERO_B
+    alert(NUMERO_A + " - " + NUMERO_B + " = " + RESULTADO)
+    console.log("El resultado de " + NUMERO_A + " - " + NUMERO_B + " es " + RESULTADO)
+}
 
-// function multiplicar() {
-//     const NUMERO_A = Number(prompt("Ingrese el primer número para multiplicar"));
-//     const NUMERO_B = Number(prompt("Ingrese el segundo número para multiplicar"));
-//     const RESULTADO = NUMERO_A * NUMERO_B
-//     alert(NUMERO_A + " x " + NUMERO_B + " = " + RESULTADO)
-//     console.log("El resultado de " + NUMERO_A + " x " + NUMERO_B + " es " + RESULTADO)
-// }
+function multiplicar() {
+    const NUMERO_A = Number(prompt("Ingrese el primer número para multiplicar"));
+    const NUMERO_B = Number(prompt("Ingrese el segundo número para multiplicar"));
+    const RESULTADO = NUMERO_A * NUMERO_B
+    alert(NUMERO_A + " x " + NUMERO_B + " = " + RESULTADO)
+    console.log("El resultado de " + NUMERO_A + " x " + NUMERO_B + " es " + RESULTADO)
+}
 
-// function dividir() {
-//     const NUMERO_A = Number(prompt("Ingrese el primer número para dividir"));
-//     const NUMERO_B = Number(prompt("Ingrese el segundo número para dividir"));
-//     const RESULTADO = NUMERO_A / NUMERO_B
-//     alert(NUMERO_A + " / " + NUMERO_B + " = " + RESULTADO)
-//     console.log("El resultado de " + NUMERO_A + " / " + NUMERO_B + " es " + RESULTADO)
-// }
+function dividir() {
+    const NUMERO_A = Number(prompt("Ingrese el primer número para dividir"));
+    const NUMERO_B = Number(prompt("Ingrese el segundo número para dividir"));
+    const RESULTADO = NUMERO_A / NUMERO_B
+    alert(NUMERO_A + " / " + NUMERO_B + " = " + RESULTADO)
+    console.log("El resultado de " + NUMERO_A + " / " + NUMERO_B + " es " + RESULTADO)
+}
 
-// function calcularIVA() {
-//     const NUMERO_A = Number(prompt("Ingrese el precio para calcular IVA (21%)"))
-//     const RESULTADO = NUMERO_A * 1.21
-//     alert("El precio con IVA es de $" + RESULTADO)
-//     console.log("El resultado del calculo del IVA es de "+ RESULTADO)
-// }
+function calcularIVA() {
+    const NUMERO_A = Number(prompt("Ingrese el precio para calcular IVA (21%)"))
+    const RESULTADO = NUMERO_A * 1.21
+    alert("El precio con IVA es de $" + RESULTADO)
+    console.log("El resultado del calculo del IVA es de "+ RESULTADO)
+}
 
-// function calcularDescuento() {
-//     const NUMERO_A = Number(prompt("Ingrese el precio para calcular el descuento (15%)"))
-//     const RESULTADO = NUMERO_A - (NUMERO_A * 0.15)
-//     alert("El precio con descuento es de $" + RESULTADO)
-//     console.log("El resultado del calculo del descuento es de "+ RESULTADO)
-// }
+function calcularDescuento() {
+    const NUMERO_A = Number(prompt("Ingrese el precio para calcular el descuento (15%)"))
+    const RESULTADO = NUMERO_A - (NUMERO_A * 0.15)
+    alert("El precio con descuento es de $" + RESULTADO)
+    console.log("El resultado del calculo del descuento es de "+ RESULTADO)
+}
 
-// do {
-//     opcion = Number(prompt("¿Qué operación quiere realizar? \n 1-Suma ➕ \n 2-Resta ➖ \n 3-Multiplicación ✖️ \n 4-División ➗ \n 5-Calcular IVA (21%) 🖨️ \n 6-Calcular descuento (15%) 🤑 \n 7-Salir ⚠️"))
+do {
+    opcion = Number(prompt("¿Qué operación quiere realizar? \n 1-Suma ➕ \n 2-Resta ➖ \n 3-Multiplicación ✖️ \n 4-División ➗ \n 5-Calcular IVA (21%) 🖨️ \n 6-Calcular descuento (15%) 🤑 \n 7-Salir ⚠️"))
 
-//     switch (opcion) {
-//         case 1:
-//             sumar()
-//             break;
-//         case 2:
-//             restar()
-//             break;
-//         case 3:
-//             multiplicar()
-//             break;
-//         case 4:
-//             dividir()
-//             break;
-//         case 5:
-//             calcularIVA() 
-//             break;
-//         case 6:
-//             calcularDescuento()
-//             break;
-//         case 7: 
-//             alert("Muchas gracias por participar👋");
-//             break;
-//         default:
-//             alert("Opción incorrecta, elegí una opción disponible");
-//             break;
-//     }
-// }while (opcion !==7)
+    switch (opcion) {
+        case 1:
+            sumar()
+            break;
+        case 2:
+            restar()
+            break;
+        case 3:
+            multiplicar()
+            break;
+        case 4:
+            dividir()
+            break;
+        case 5:
+            calcularIVA() 
+            break;
+        case 6:
+            calcularDescuento()
+            break;
+        case 7: 
+            alert("Muchas gracias por participar👋");
+            break;
+        default:
+            alert("Opción incorrecta, elegí una opción disponible");
+            break;
+    }
+}while (opcion !==7)
 
-// alert("🚩En el caso de haber realizado alguna operación, los resultados quedaron grabados en la consola.👨🏻‍💻 \n¡Hasta la próxima!👋")
+alert("🚩En el caso de haber realizado alguna operación, los resultados quedaron grabados en la consola.👨🏻‍💻 \n¡Hasta la próxima!👋")
 
 
 // /*************************************       JUEGO: CODIGO DE DESCUENTO              *****************************/
 // /*                                         (Condicionales + Metodo Random)                                       */
 
-// let intentosRestantes = 3;
+let intentosRestantes = 3;
 
-// do {
-//   let eleccionDelJugador = prompt("Ingrese su número entre 1 y 5, acertá el número y obtené un código de descuento. Tiene 3 intentos");
-//   eleccionDelJugador = parseInt(eleccionDelJugador)
+do {
+  let eleccionDelJugador = prompt("Ingrese su número entre 1 y 5, acertá el número y obtené un código de descuento. Tiene 3 intentos");
+  eleccionDelJugador = parseInt(eleccionDelJugador)
 
-//   let numeroGanador = Math.floor(Math.random() * 5) + 1;
-//   console.log("Número seleccionado: ", eleccionDelJugador);
-//   console.log("Número ganador: ", numeroGanador);
+  let numeroGanador = Math.floor(Math.random() * 5) + 1;
+  console.log("Número seleccionado: ", eleccionDelJugador);
+  console.log("Número ganador: ", numeroGanador);
 
-//   if (isNaN(eleccionDelJugador) || eleccionDelJugador < 1 || eleccionDelJugador > 5) {            //Si pone un número o valor incorrecto..
-//     alert("Por favor, ingrese un número entre 1 y 5")
-//     }
-//   else if (eleccionDelJugador === numeroGanador){                                                 //Si pone el número ganador...
-//     alert("🔥 ¡Felicitaciones, acertaste! 🔥 El código de descuento es 'GaneElDesafio'");
-//     break;
-//     }
-//   else {                                                                                          //Si no acierta el número ganador..
-//     if (intentosRestantes > 1) {                                                                  //.. y todavía tienen intentos disponibles..
+  if (isNaN(eleccionDelJugador) || eleccionDelJugador < 1 || eleccionDelJugador > 5) {            //Si pone un número o valor incorrecto..
+    alert("Por favor, ingrese un número entre 1 y 5")
+    }
+  else if (eleccionDelJugador === numeroGanador){                                                 //Si pone el número ganador...
+    alert("🔥 ¡Felicitaciones, acertaste! 🔥 El código de descuento es 'GaneElDesafio'");
+    break;
+    }
+  else {                                                                                          //Si no acierta el número ganador..
+    if (intentosRestantes > 1) {                                                                  //.. y todavía tienen intentos disponibles..
     
-//        do {                                                                                        //Hará...
-//        continuar = prompt("No coincide 😥\n¿Desea seguir intentando? (si/no)").toLowerCase();     //Preguntar al usuario si quiere seguir.
+       do {                                                                                        //Hará...
+       continuar = prompt("No coincide 😥\n¿Desea seguir intentando? (si/no)").toLowerCase();     //Preguntar al usuario si quiere seguir.
   
-//        if (continuar !== "si" && continuar !== "no") {                                           //Si el usuario no selecciona "si" ni  "no"..
-//         alert("Por favor, ingrese 'si' o 'no'.");
-//        }
-//       } while (continuar !== "si" && continuar !== "no");                                           //..vuelve a preguntar si quiere seguir 
+       if (continuar !== "si" && continuar !== "no") {                                           //Si el usuario no selecciona "si" ni  "no"..
+        alert("Por favor, ingrese 'si' o 'no'.");
+       }
+      } while (continuar !== "si" && continuar !== "no");                                           //..vuelve a preguntar si quiere seguir 
     
-//       if (continuar === "no") {                                                                     //Si el usuario selecciona "no"..
-//        alert("¡En otra ocasión lo intentaremos! 👋");
-//        break;
-//       }
-//     }
-//     else {                                                                                        //Si vuelve a errar y no hay más intentos..
-//       alert("No acertaste😥\nSe acabaron los intentos. ¡Mejor suerte la próxima vez! 👋");
-//       break;
-//     }
+      if (continuar === "no") {                                                                     //Si el usuario selecciona "no"..
+       alert("¡En otra ocasión lo intentaremos! 👋");
+       break;
+      }
+    }
+    else {                                                                                        //Si vuelve a errar y no hay más intentos..
+      alert("No acertaste😥\nSe acabaron los intentos. ¡Mejor suerte la próxima vez! 👋");
+      break;
+    }
 
-//   intentosRestantes--;                                                                                //Descontará un intento por cada fallo.
-// }
+  intentosRestantes--;                                                                                //Descontará un intento por cada fallo.
+}
 
-// } while (intentosRestantes > 0)                                                        //Repetirá el bucle mientras haya intentos disponibles
+} while (intentosRestantes > 0)                                                        //Repetirá el bucle mientras haya intentos disponibles
 
 /*************************************         CATALOGO DE PRODUCTOS                 *****************************/
 /*                                          (Constructor + Metodo Push)                                          */
